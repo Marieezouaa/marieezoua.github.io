b6a63c1f529180a352c38bb14f0ca80bdc302e4b
Marieezouaa <88006867+Marieezouaa@users.noreply.github.com>
2025-05-20 13:36:04 -0400

    github pages base correction

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/marieezoua.github.io/",
+  base: "/marieezouaa.github.io/",
 });
